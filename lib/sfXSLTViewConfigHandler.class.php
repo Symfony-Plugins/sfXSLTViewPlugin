4e19baf0060bf87fa16f11e53fb7ca95ed2277e2
johnwards <johnwards@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-02-17 09:34:02 -0600

    xslt.yml config files ability
    git-svn-id: http://svn.symfony-project.com/plugins/sfXSLTViewPlugin/trunk@15564 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/sfXSLTViewConfigHandler.class.php
@@ -0,0 +1,24 @@
+<?php
+
+class sfXSLTViewConfigHandler extends sfYamlConfigHandler {
+  
+  public function __construct($parameters = null)
+  {
+    $this->initialize($parameters);
+  }
+  
+  public function execute($configFiles){
+    
+    $config = $this->parseYamls($configFiles);
+    
+    $data  = "<?php\n";
+    if(isset($config["keytests"]) && is_array($config["keytests"])){
+      foreach($config["keytests"] as $key => $test)
+      {
+        $data .= sprintf("sfArray2XML::\$key_test[\"%s\"]=\"%s\";\n",$key,$test);
+      }
+    }
+    
+    return $data;
+  }
+}
